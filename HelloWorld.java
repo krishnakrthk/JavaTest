7c1865f0ebc971aff145baa3e93cbbbddec7ac4a
Krishna <krishnakrthk@gmail.com>
2016-04-10 22:06:40 +0530

    Example staging

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World");
 	System.out.println("Hello, INDIA");
-	System.out.println("Hello, TamilNaduNewerchaged");
+
+	System.out.println("Hello, TamilNadu");
+
     }
 
 }
